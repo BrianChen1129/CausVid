a5870958497d02cf695a22b2e26d53b09590d595
“BrianChen1129” <yongqich@umich.edu>
2025-07-13 22:22:19 +0000

    Remove local_scripts from repo and add to .gitignore

--- local_scripts/debug.sh
+++
@@ -1,3 +0,0 @@
-export WANDB_MODE=offline
-torchrun --nnodes 1 --nproc_per_node=1 --master-port 29501 causvid/train_distillation.py \
-    --config_path  local_scripts/wan_bidirectional_dmd_from_scratch_debug.yaml 
